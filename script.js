23b3f204f309a628b31c346afff4877970367826
Adetayo Alexander <97042825+Lexcodes001@users.noreply.github.com>
2022-07-02 16:37:14 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+var currentDate = new Date();
+
+const days = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'];
+
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
+document.getElementById('time').innerHTML = currentDate.getHours() + ':' + currentDate.getMinutes();
+
+document.getElementById('seconds').innerHTML = currentDate.getSeconds();
+
+document.getElementById('date').innerHTML= days[currentDate.getDay()] + ' ' + months[currentDate.getUTCMonth()] + ' ' + currentDate.getDate() + ', ' + currentDate.getFullYear();
+
